14e0e58a33299746f3b09acf296be1ceab3de2b6
hubte1g <lickteig.joshua@gmail.com>
2016-06-13 15:28:20 -0500

    Update hdfs.sh

--- hdfs.sh
+++ hdfs.sh
@@ -2,4 +2,8 @@
 hdfs dfs -ls -R / | sort -r -n -k 5
 
 # By top 10
-hdfs dfs -ls -R /kohls/eim/ | sort -r -n -k 5 | head -10
+hdfs dfs -ls -R /dir/dir_/ | sort -r -n -k 5 | head -10
+
+# count lines in part file
+hdfs dfs -cat /dir/dir_/dir__/dir___/dir____/part-m-00000 | wc -l
+
