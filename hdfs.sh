89d84a95a118aa24a48caeb325f606eeffcc4361
hubte1g <lickteig.joshua@gmail.com>
2016-07-15 10:09:49 -0500

    Update hdfs.sh

--- hdfs.sh
+++ hdfs.sh
@@ -7,3 +7,6 @@ hdfs dfs -ls -R /dir/dir_/ | sort -r -n -k 5 | head -10
 # count lines in part file
 hdfs dfs -cat /dir/dir_/dir__/dir___/dir____/part-m-00000 | wc -l
 
+# display hdfs as tree
+hadoop fs -lsr /dir | awk '{print $8}' | sed -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'
+
